d5a20477e133068a3ceca438c3ea5a10dccf52ed
Mayank-p19 <mayankpunia74@gmail.com>
2024-10-29 11:34:46 +0530

    users data

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 import connectMongoDB from "./db/connectMongoDB.js"
 
 dotenv.config();
 
 const app = express();
-
 const PORT = process.env.PORT || 5000;
 
 console.log(process.env.MONGO_URI);
 
-app.use(express.json());
+app.use(express.json());    //to parse req.body
+app.use(express.urlencoded({ extended: true })); //to parse form data(urlencoded)
 
-app.use("/auth/routes", authRoutes);
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
