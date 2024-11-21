f24a691ecb30fae9026cb7a2cbb412ccf71fbcf2
Mayank-p19 <mayankpunia74@gmail.com>
2024-11-21 21:28:48 +0530

    user routes and controllers added

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,12 +1,12 @@
 import express from "express";
 import { protectRoute } from '../middleware/protectroute.js';
-import { getUserProfile, followUnfollowUser,  } from "../controllers/user.controller.js";
+import { getUserProfile, followUnfollowUser, getSuggestedUsers, updateUserProfile } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.get("/profile/:username", protectRoute, getUserProfile);
-// router.get("/suggested", protectRoute, getUserProfile);
+router.get("/suggested", protectRoute, getSuggestedUsers);
 router.post("/follow/:id", protectRoute, followUnfollowUser);
-// router.post("/update", protectRoute, updateUserProfile);
+router.post("/update", protectRoute, updateUserProfile);
 
 export default router;
